d5a2ddc47ed9dcf02ad137435d43aba08ba77945
Soundariya5 <89157135+Soundariya5@users.noreply.github.com>
2022-05-04 08:12:54 +0530

    Add files via upload

---
+++ MyCar.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		Car object=new Car();
+		object.applyBreak();
+		object.applyGear(5);
+		object.applyAccelerate(0);
+		object.switchonAc(1);
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ MyMoblieClass.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class MyMoblieClass {
+
+	public static void main(String[] args) {
+		MobileClass mymob=new MobileClass();
+		mymob.sendMessage();
+		mymob.shareDocument();
+		mymob.call();
+		// TODO Auto-generated method stub
+
+	}
+
+}
