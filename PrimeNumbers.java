b130740b4e0a72c468ef08f001fa96a21e84ccb9
Soundariya5 <89157135+Soundariya5@users.noreply.github.com>
2022-05-05 08:56:07 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,24 @@
+package assignments;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		int i,a=1,count;
+		while(a<=10) {
+			count=0;
+			i=2;
+			while(i<=a/2) {
+				if(a%i==0) {
+					count++;
+					break;
+				}
+				i++;
+				}
+			if(count==0&&a!=1) {
+				System.out.println(a);
+		}
+			a++;
+		}
+	}
+
+}
