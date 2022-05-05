1cc31d549d6e6a19643e04c1bff6e662f26cf5d1
Soundariya5 <89157135+Soundariya5@users.noreply.github.com>
2022-05-05 10:59:58 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,10 +6,10 @@ public class FibonacciSeries {
 		int a=0;
 		int b=1;
 		for(int i=1;i<=10;i++) {
-       int c=a+b;
-       a=b;
-       b=c;
-		System.out.println(b);
+                int c=a+b;
+		System.out.println(a);
+	        a=b;
+                b=c;
 		// TODO Auto-generated method stub
 		}
 	}
